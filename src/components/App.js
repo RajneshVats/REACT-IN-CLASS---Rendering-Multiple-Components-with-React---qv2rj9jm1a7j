8fd241accee88aff3e2ca2a731191f7430da9e98
RajneshVats <93943698+RajneshVats@users.noreply.github.com>
2022-02-08 13:10:01 +0530

    s

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <h1 data-ns-test="project-name">Desi Myntra</h1>
+      <h2 data-ns-test="project-description">Have desi products for desi people.</h2>
+    </div>
   )
 }
 
